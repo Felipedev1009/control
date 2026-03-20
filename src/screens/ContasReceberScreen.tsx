4f27a8a6d6fac16402d9a7c31731462fb720f065
Felipe <felipermdomingos@gmail.com>
2026-03-19 22:11:08 -0300

    Telas Iniciais

---
+++ src/screens/ContasReceberScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function RegisterScreen() {
+    return (
+        <View>
+            <Text>Contas a Receber Screen</Text>
+            </View>
+    )
+}
